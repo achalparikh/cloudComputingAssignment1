3e5c7400aef602356d75a7a197b202745177c096
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2018-02-05 12:08:49 -0500

    Added checks for 0 values

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ def stringToFloat(value):
     v = float(value)
     return v
 
+#home route: Displays the index form for input
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+#success route displays the results store in the db
 @app.route('/success', methods = ['POST'])
 def success():
     if request.method == 'POST':
         email = request.form["user_email"]
         height = stringToFloat(request.form["user_height"])
         weight = stringToFloat(request.form["user_weight"])
-        if (height != 0) and (weight != 0):
+        if (height != 0) and (weight > 0):#if entered height is 0 and weight is greater than 0
             bmi = weight/(height*height)
+            bmi = ("%.2f" % bmi)
             db.insert(email, height, weight, bmi)
             print(db.search(email))
             return render_template('success.html', email=db.search(email)[-1][1], height=db.search(email)[-1][2], weight=db.search(email)[-1][3]
