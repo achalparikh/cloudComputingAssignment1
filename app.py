cfd89fb96d55c8fe4623a8a465a755a5941278b1
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2018-02-07 16:48:44 -0500

    Commented Code

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+Author: Achal Parikh
+ID: 100520618
+Description: This python program asks users for their email address, height, and weight and calculates the BMI using a HTML pages
+"""
 from flask import Flask, render_template, request
 import db
 
@@ -15,6 +20,7 @@ def index():
 #success route displays the results store in the db
 @app.route('/success', methods = ['POST'])
 def success():
+    #post method is used to receive user data to the backend for BMI Calculation
     if request.method == 'POST':
         email = request.form["user_email"]
         height = stringToFloat(request.form["user_height"])
@@ -22,7 +28,9 @@ def success():
         if (height != 0) and (weight > 0):#if entered height is 0 and weight is greater than 0
             bmi = weight/(height*height)
             bmi = ("%.2f" % bmi)
+            #stores data into the database
             db.insert(email, height, weight, bmi)
+            #prints data from the database
             print(db.search(email))
             return render_template('success.html', email=db.search(email)[-1][1], height=db.search(email)[-1][2], weight=db.search(email)[-1][3]
                                    , bmi=db.search(email)[-1][4])
